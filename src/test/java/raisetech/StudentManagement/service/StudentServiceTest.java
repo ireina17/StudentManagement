195f34cccd3f069575a4c643632c415309899366
ireina17 <kui07iku665l@yahoo.co.jp>
2025-04-25 11:57:53 +0900

    40回 テストコード追加,修正

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,7 +16,6 @@ import raisetech.StudentManagement.repository.StudentRepository;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -40,8 +40,6 @@ class StudentServiceTest {
         //事前準備
         List<Student> studentList = new ArrayList<>();
         List<StudentCourse> studentCourseList = new ArrayList<>();
-
-
         when(repository.search()).thenReturn(studentList);
         when(repository.searchStudentCourseList()).thenReturn(studentCourseList);
 
@@ -52,29 +50,58 @@ class StudentServiceTest {
         verify(repository, Mockito.times(1)).search();
         verify(repository, Mockito.times(1)).searchStudentCourseList();
         verify(converter, Mockito.times(1)).convertStudentDetails(studentList, studentCourseList);
+    }
 
-        //----------------課題----------------
+    //----------------課題----------------
+
+    //searchStudent
+    @Test
+    void 受講生検索_リポジトリとコンバーターの処理が適切に呼び出せてること() {
+
         //事前準備
+        String id = "999";
         Student student = new Student();
-        student.setId("1");
+        student.setId(id);
+        when(repository.searchStudent(id)).thenReturn(student);
+        when(repository.searchStudentCourse(id)).thenReturn(new ArrayList<>());
 
-        StudentDetail studentDetail = new StudentDetail();
-        when(repository.searchStudent("1")).thenReturn(student);
+        StudentDetail expected = new StudentDetail(student, new ArrayList());
 
-        studentDetail.setStudent(student);
-        studentDetail.setStudentCourseList(studentCourseList);
+        //実行
+        StudentDetail actual = sut.searchStudent(id);
+
+        //検証
+        verify(repository, Mockito.times(1)).searchStudent(id);
+        verify(repository, Mockito.times(1)).searchStudentCourse(id);
+        Assertions.assertEquals(expected.getStudent().getId(), actual.getStudent().getId());
+    }
+
+    //registerStudent
+    @Test
+    void 受講生詳細登録_リポジトリとコンバーターの処理が適切に呼び出せてること() {
+        Student student = new Student();
+        StudentCourse studentCourse = new StudentCourse();
+        List<StudentCourse> studentCoursesList = List.of(studentCourse);
+        StudentDetail studentDetail = new StudentDetail(student, studentCoursesList);
 
         //実行
         sut.registerStudent(studentDetail);
 
-        //検証
         verify(repository, Mockito.times(1)).registerStudent(student);
+        verify(repository, Mockito.times(1)).registerStudentCourse(studentCourse);
+    }
+
+    @Test
+    void 受講生詳細更新_リポジトリとコンバーターの処理が適切に呼び出せてること() {
+        Student student = new Student();
+        StudentCourse studentCourse = new StudentCourse();
+        List<StudentCourse> studentCoursesList = List.of(studentCourse);
+        StudentDetail studentDetail = new StudentDetail(student, studentCoursesList);
 
         //実行
-        StudentDetail actual = sut.searchStudent("1");
+        sut.updateStudent(studentDetail);
 
-        //検証
-        assertEquals(student, actual.getStudent());
-        verify(repository, Mockito.times(1)).searchStudent("1");
+        verify(repository, Mockito.times(1)).updateStudent(student);
+        verify(repository, Mockito.times(1)).updateStudentCourse(studentCourse);
     }
 }
