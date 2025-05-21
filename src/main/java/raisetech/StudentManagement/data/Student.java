6529683b9259a3726bfce3056bc929bfaa4fd8a4
ireina17 <kui07iku665l@yahoo.co.jp>
2025-05-21 15:07:54 +0900

    importのワールドカードから個別に設定

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -3,7 +3,11 @@ package raisetech.StudentManagement.data;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
 @Schema(description = "受講生")
--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -2,7 +2,11 @@ package raisetech.StudentManagement.domain;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 
