4e8c7a72c74ae3e10bb8b089cb4149be23c46acc
ireina17 <kui07iku665l@yahoo.co.jp>
2025-06-09 18:31:57 +0900

    別スローに変更した為、削除

--- src/main/java/raisetech/StudentManagement/exception/CourseStatusNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package raisetech.StudentManagement.exception;
-
-public class CourseStatusNotAllowedException extends RuntimeException {
-    public CourseStatusNotAllowedException() {
-        super("入力されたコースステータスは許可されていません。仮申込／本申込／受講中／受講終了を入力してください。");
-    }
-}
